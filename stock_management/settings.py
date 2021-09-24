b9bc666caf1c1a5cad4fc009a5619ed90d5a1aea
Gopikrishnan V <56466603+gopikrishvs@users.noreply.github.com>
2021-09-24 17:10:36 +0530

    Update settings.py

--- stock_management/settings.py
+++ stock_management/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'ico37a^ca!(zl+f6(s(0jgd9ug4*05*3-#xdscsvz)n&yeh0ge'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['gopi-stock-mgmt.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
